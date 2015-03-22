ac261ee5e5d0a760270efd7a890fdd85d891d450
saurabh kumar <saurabh.nitc10@gmail.com>
2015-03-22 21:44:42 +0530

    icon modification on div expand and collapse

--- js/resultEvenHandler.js
+++ js/resultEvenHandler.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
-	$(".chomp--link").click(function(){
+	$(".chomp--link,.tile__expand ").click(function(){
 		$content = $(".zci__content");
 		if($(".zci").hasClass("has-chomp-expanded")){
 			$(".zci").removeClass("has-chomp-expanded");
 			$(".chomp--link__mr").show();
 			$(".chomp--link__ls").hide();
+			$(".tile").removeClass("is-open");
 		}
 		else{
 			$(".zci").addClass("has-chomp-expanded");
 			$(".chomp--link__mr").hide();
 			$(".chomp--link__ls").show();
-		}
-		
-		
-		
-		
+			$(".tile").addClass("is-open");
+		}		
 	})
 });
